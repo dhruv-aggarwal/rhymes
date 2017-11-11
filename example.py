1c08e85110f19546cff39278809f18e48a5f1dd8
Dhruv Aggarwal <dhruvaggarwal043@gmail.com>
2017-11-12 02:12:54 +0530

    Add example rhymes

---
+++ example.py
@@ -0,0 +1,44 @@
+examples = [
+    """Whose woods these are I think I know.
+    His house is in the village, though;
+    He will not see me stopping here
+    To watch his woods fill up with snow.""",
+    """Two roads diverged in a yellow wood,
+    And sorry I could not travel both
+    And be one traveler, long I stood
+    And looked down one as far as I could
+    To where it bent in the undergrowth;""",
+    """I want to run, I want to hide
+    From all the pain he caused inside.
+    I want to scream, I want to cry.
+    Why can't I tell him goodbye?
+    I want to move on; I just can't let go.
+    I love him more than he will ever know.
+    I want to start over, I want to feel free!
+    But this pain will never leave me be.
+    He hurt me bad; the pain is deep
+    From all the promises he couldn't keep.
+    All the lies I heard him say
+    Are in my head and just won't fade.
+    How can I forget him, leave him behind?
+    Erase the memories from my mind?
+    He doesn't love me, and he never will.
+    He will never care how I feel.""",
+    """I was angry with my friend:
+    I told my wrath, my wrath did end.
+    I was angry with my foe:
+    I told it not, my wrath did grow.
+    And I watered it in fears
+    Night and morning with my tears;
+    And I sunned it with smiles,
+    And with soft deceitful wiles.""",
+    """The one who brought me down to earth
+    And held me every day.
+    The one who gracefully gave me birth,
+    And said, I love you in every way.
+
+    The one who taught me everything,
+    Like how to crawl and walk.
+    The one who taught me how to sing
+    After learning how to talk."""
+]
